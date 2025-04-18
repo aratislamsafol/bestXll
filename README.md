3a867f3c0afd23b76f7487b3d68edc3028f7f2cf
aratislamsafol <aratislamsafol@gmail.com>
2025-04-18 10:42:08 +0600

    environment setup done with tailwind css

---
+++ README.md
@@ -0,0 +1,1 @@
+Best Xll Select React Simple Project
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <p className="text-3xl font-bold">Checked</p>
+    </>
+  )
+}
+
+export default App
