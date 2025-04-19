7ec00d20cecaf4ab2f539ef01625c45e181d004a
aratislamsafol <aratislamsafol@gmail.com>
2025-04-19 19:45:31 +0600

    Footer issuses solved

---
+++ src/Components/Footer/Copywrite.jsx
@@ -0,0 +1,7 @@
+export default function Copywrite() {
+  return (
+    <div className="text-center border-t border-gray-700 p-6">
+        <h5 className="text-sm md:text-base text-gray-600">@2025 Your Company All Rights Reserved.</h5>
+    </div>
+  )
+}
